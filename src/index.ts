de999c1df1a8d89fe672a58e0c6945d9a3069156
MiguelMendezNoBot <miguelgonzalescm8@gmail.com>
2025-10-10 22:28:28 -0400

    feat: use environment variables for MongoDB configuration

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 import Server from './config/server.config';
-import { SERVER_PORT } from './config/env.config';
+import { SERVER_PORT, MONGODB_URI } from './config/env.config';
 import mongoose from 'mongoose';
 
 async function startServer() {
   try {
-    await mongoose.connect(
-      //"mongodb+srv://admin:admin123@cluster-if.mamcwj7.mongodb.net/Internal-Func-BD?retryWrites=true&w=majority&appName=Cluster-IF"
-      //"mongodb+srv://admin:qwerty123@clusterservineo.yotr2ip.mongodb.net/ServineoBD?retryWrites=true&w=majority&appName=ClusterServineo"
-      //"mongodb+srv://test123:test321@clusterservineo.yotr2ip.mongodb.net/ServineoBD?retryWrites=true&w=majority&appName=ClusterServineo"
-      //"mongodb+srv://loremipsum:FY560KjK7KGsO6BG@clusterservineo.yotr2ip.mongodb.net/ServineoBD?retryWrites=true&w=majority&appName=ClusterServineo"
-      "mongodb+srv://sudoers:mOG7495o6IH4Nnjf@clusterservineo.yotr2ip.mongodb.net/ServineoBD?retryWrites=true&w=majority&appName=ClusterServineo"
-    );
-    
+    await mongoose.connect(MONGODB_URI);
     console.log("Connected to database MongoDB Atlas!");
     
     Server.listen(SERVER_PORT, () => {
