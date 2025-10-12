f7325a887bd847de25cdd99be8e8107d72f53a80
Daynor Montoya Chura <montoya.chura.daynor@gmail.com>
2025-10-12 13:06:59 -0400

    Add database connection setup

---
+++ src/serverData.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const MONGO_DB_URI = process.env.MONGO_DB_URI as string;
+let comida: String = 'comida';
+export const connectDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect(MONGO_DB_URI, {});
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
