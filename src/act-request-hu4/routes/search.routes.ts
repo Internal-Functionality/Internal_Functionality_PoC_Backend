7aba5864bffba174b562d2e106b2ea32de98e30e
Daynor Montoya Chura <montoya.chura.daynor@gmail.com>
2025-10-12 13:13:17 -0400

    Add files with API routes

---
+++ src/act-request-hu4/routes/search.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import * as searchController from '../controllers/search.controller';
+const router = Router();
+
+router.get('/read', searchController.readSearch); // GET /api/search
+router.get('/read', searchController.readSearch); // GET /api/search
+router.post('/create', searchController.createSearch); // POST /api/search/capture
+router.put('/update', searchController.updateSearch); // PUT /api/search/123
+router.delete('/delete', searchController.deleteSearch); // DELETE /api/search/123
+
+export default router;
