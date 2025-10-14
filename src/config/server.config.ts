3506dd616c1e91593a9b3d9c1651aaf7e05f9982
German Flores <gestebanflortander@gmail.com>
2025-10-14 16:30:36 -0400

    Add session end record

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -3,7 +3,14 @@ import cors from 'cors';
 import AppRoutes from './server.routes';
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:3001", // origen permitido (tu frontend)
+    credentials: true, // permite cookies, headers de autenticación, etc.
+  })
+);
+
 app.use(express.json());
 app.use(AppRoutes);
 
--- src/visitors/routes/auth.ts
+++ src/visitors/routes/auth.ts
@@ -3,7 +3,7 @@ import { createOrResumeVisitor, getVisitorById } from "../controllers/visitor";
 
 const router = Router();
 
-router.post("/visitor", createOrResumeVisitor);
-router.get("/visitor/:id", getVisitorById);
+router.post("/", createOrResumeVisitor);
+router.get("/:id", getVisitorById);
 
 export default router;
