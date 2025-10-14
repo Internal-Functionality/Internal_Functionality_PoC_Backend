066ef5c0cc49a894e752d0d49759025f8dcc7c7a
Elsebas7 <sebastianlakkarivera7@gmail.com>
2025-10-13 22:02:27 -0400

    Merge brach  'dev' into feature/FixerActivity

--- src/config/server.routes.ts
+++ src/config/server.routes.ts
@@ -1,23 +1,17 @@
 import { Router } from 'express';
 import HealthRoutes from '../modules/health/health.routes';
-<<<<<<< HEAD
-import ActivityRoutes from '../fixer-activity/routes/activity.routes';
-=======
 import UserRoutes from '../act-requesters-hu6/routes/user.routes';
 import JobRoutes from '../act-requesters-hu6/routes/job.route';
 import ActivitRoutes from '../act-requesters-hu6/routes/activity.route';
->>>>>>> dev
+import ActivityRoutes from '../fixer-activity/routes/activity.routes';
 
 const router = Router();
 
 router.use('/api', HealthRoutes);
-<<<<<<< HEAD
-router.use('/api', ActivityRoutes);
-=======
 router.use('/api', UserRoutes);
 router.use('/api', JobRoutes);
 router.use('/api', ActivitRoutes);
->>>>>>> dev
+router.use('/api', ActivityRoutes);
 
 router.use((req, res) => {
   console.log('Not found:', req.method, req.originalUrl);
