9ecb6c8044da12cac9db2013c03116d8cb3e7884
Elsebas7 <sebastianlakkarivera7@gmail.com>
2025-10-14 00:17:01 -0400

    conflict resolved

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,18 @@
 import Server from './config/server.config';
-<<<<<<< HEAD
-import { connectDB } from './config/database.config';
-
-import { SERVER_PORT } from './config/env.config';
-
-async function startServer() {
-  try {
-    await connectDB();
-=======
 import { SERVER_PORT, MONGODB_URI } from './config/env.config';
 import mongoose from 'mongoose';
 
 async function startServer() {
   try {
     await mongoose.connect(MONGODB_URI);
-    console.log("Connected to database MongoDB Atlas!");
-    
->>>>>>> dev
+    console.log('Connected to database MongoDB Atlas!');
+
     Server.listen(SERVER_PORT, () => {
       console.info(`Server running on http://localhost:${SERVER_PORT}`);
     });
-    
   } catch (error) {
     console.error('Error starting server', error);
   }
 }
 
-<<<<<<< HEAD
 startServer();
-=======
-startServer();
->>>>>>> dev
